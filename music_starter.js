3c046a37218dbe22e2864b60ae0befa139174ad5
ashtonguyy <ashtonguy4413@gmail.com>
2023-10-20 16:51:58 +1300

    Updated Code
    Current Code

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,37 @@
-
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+  background(20);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+  var pointsize = 10;
+  var vocalheight = map(vocal, 0, 100, height / 2, height / 2 - 200 )
+  var bassheight = map(bass, 0, 100, height / 2, height / 2 + 200 )
+  var drumwidthL = map(drum, 0, 100, width / 2, width / 2 + 200)
+  // Drawing a large even cross
+  stroke(127, 255, 212); // hospital green
+  line(width / 2 - 200, height / 2, width / 2 + 200, height / 2); // Horizontal line
+  line(width / 2, height / 2 - 200, width / 2, height / 2 + 200); // Vertical line
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  //DRUM variable for oscilloscope LEFT
+  fill(127, 255, 212); //hospital green
+  ellipse(height / 2, drumwidthL, pointsize);
+
+  //DRUM variable for oscilloscope RIGHT
+  fill(127, 255, 212); //hospital green
+  ellipse(width / 2 + drum, height / 2 - drum, pointsize);
+
+  //VOCAL variable for oscilloscope
+  fill(127, 255, 212); //hospital green
+  ellipse(width/2, vocalheight, pointsize);
+
+  //BASS variable for oscilloscope CENTER
+  fill(127, 255, 212); //hospital green
+  ellipse(width / 2, bassheight, pointsize)
+
+  //BASS variable for oscilloscope LEFT
+  fill(127, 255, 212); //hospital green
+  ellipse(width / 2 - bass, height / 2 + bass, pointsize)
+
+  //BASS variable for oscilloscope RIGHT
+  fill(127, 255, 212); //hospital green
+  ellipse(width / 2 + bass, height / 2 + bass, pointsize)
+
 }
